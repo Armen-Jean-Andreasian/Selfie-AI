77f11d74691281e363f2df6cfd2d90319216bac6
Armen-Jean-Andreasian <armen.andriasyan4@gmail.com>
2023-10-16 12:36:58 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# SelfieCamera-with-FaceRecognition App
+# Selfie-AI
 ![image](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftalkjarvis.com%2Fwp-content%2Fuploads%2F2019%2F12%2F1-1.jpg&f=1&nofb=1&ipt=f97256ba48e23d92004bcc7c20e20b8e54ced69c68c2cb85c6fc1e7d99743414&ipo=images)
 
-This is a SelfieCamera app that incorporates Face Recognition capabilities. 
+This is a selfie camera app that incorporates Face Recognition capabilities. 
 The app allows users to capture selfies using their device's camera and apply face recognition to identify faces in the images. 
 The app is built using openCV2 and Streamlit (simulator of getting random errors). 
 
@@ -24,48 +24,42 @@ To use the SelfieCamera-with-FaceRecognition app, follow these steps:
 1. Clone the repository to your local machine:
 
    ```bash
-   
    git clone https://github.com/your_username/SelfieCamera-with-FaceRecognition.git
-
+   ```
 
 2. Navigate to the project directory:
 
     ```bash
-    cd SelfieCamera-with-FaceRecognition```
-
-
+    cd SelfieCamera-with-FaceRecognition
+   ```
+   
 3. Install the required dependencies. The app requires the following packages:
 
 - cv2
 - streamlit
-
-
-Install these packages using the following command:
-
-```bash
-pip install cv2 streamlit 
-``` 
-
-Run the app using the following command:
-
-```bash
-streamlit run main.py
-```
+  > You can install these packages using the following command:
+  > ```bash
+  > pip install cv2 streamlit
+  > ```
+  > 
+  > Run the app using the following command:
+  > ```bash
+  > streamlit run main.py
+  > ```
 
 This will start the app, and you can access it in your web browser at http://localhost:8501.
 
+---
 ## Deployment Notes
 
 Notes: 
-- If you want to deploy this app not on Heroku but on Streamlit, I wish you to stay strong 
-and good luck, of course if you are not a master of Streamlit. Have fun.
+- If you want to deploy this app not on Heroku but on Streamlit:
+  - Streamlit support suggests to include `opencv-python-headless ` to the `requirements.txt`
+  - However, if you want to completely overcome the issues you can try to use `streamlit-webrtc`
 
-- Streamlit support suggests to include `opencv-python-headless ` to the `requirements.txt`
-- However, if you want to completely overcome the issues you can try to use `streamlit-webrtc`
-
-
+---
 ## Contributing
-I welcome contributions to the SelfieCamera-with-FaceRecognition app! To contribute, please follow these steps:
+I welcome contributions to the Selfie AI app! To contribute, please follow these steps:
 
 1. Fork the repository on GitHub.
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import cv2
-from face_recognition import detect_faces
-from options import time_stamp, live_image, download_image
+from face_recognition import Recognize
+from options import Options
 
 
 class PhotoAPP:
@@ -11,42 +11,42 @@ class PhotoAPP:
         self.image_widget = None
         self.shot_button = None
 
+    def initialize_camera(self):
+        self.camera = cv2.VideoCapture(0)
+        self.image_widget = st.image([])
+        self.shot_button = st.button("Snap")
+        st.selectbox("", options=("Enable date", "Disable date"), key="user_date_choice", index=0)
+        st.selectbox("", options=("Enable recognition", "Disable recognition"), key="user_recognition_choice", index=0)
+
+    def capture_frame(self):
+        while True:
+            check, current_frame = self.camera.read()
+            current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2RGB)
+            if st.session_state["user_date_choice"] == "Enable date":
+                Options.time_stamp(frame_to_stamp=current_frame)
+            if st.session_state["user_recognition_choice"] == "Enable recognition":
+                frame_with_detection = Recognize.detect_faces(current_frame)
+                Options.live_image(frame=self.image_widget, image=frame_with_detection)
+            elif st.session_state["user_recognition_choice"] == "Disable recognition":
+                Options.live_image(frame=self.image_widget, image=current_frame)
+            if self.shot_button and check:
+                rgb_frame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2BGR)
+                _, buffer = cv2.imencode('.jpg', rgb_frame)
+                shot = buffer.tobytes()
+                break
+        return shot
 
     def main(self):
         with st.expander("Selfie with Face Recognition"):
             self.title = st.title("Selfie with AI")
             if st.button('Start / Reload'):
-
-                st.selectbox("", options=("Enable date", "Disable date"), key="user_date_choice", index=0)
-                st.selectbox("", options=("Enable recognition", "Disable recognition"), key="user_recognition_choice",
-                             index=0)
-
-                self.camera = cv2.VideoCapture(0)
-                self.image_widget = st.image([])
-                self.shot_button = st.button("Snap")
-
-
-                while True:
-                    check, current_frame = self.camera.read()
-                    current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2RGB)
-
-                    if st.session_state["user_date_choice"] == "Enable date":
-                        time_stamp(frame_to_stamp=current_frame)
-
-                    if st.session_state["user_recognition_choice"] == "Enable recognition":
-                        frame_with_detection = detect_faces(current_frame)
-                        live_image(frame=self.image_widget, image=frame_with_detection)
-
-                    elif st.session_state["user_recognition_choice"] == "Disable recognition":
-                        live_image(frame=self.image_widget, image=current_frame)
-
-                    if self.shot_button and check:
-                        rgb_frame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2BGR)
-                        _, buffer = cv2.imencode('.jpg', rgb_frame)
-                        shot = buffer.tobytes()
-                        break
-
-                download_image(shot)
+                try:
+                    self.initialize_camera()
+                    shot = self.capture_frame()
+                    Options.download_image(shot)
+                except Exception:
+                    st.error(
+                        "Camera not detected: Please ensure that the camera is connected and that camera permissions are set correctly.")
 
 
 if __name__ == '__main__':
